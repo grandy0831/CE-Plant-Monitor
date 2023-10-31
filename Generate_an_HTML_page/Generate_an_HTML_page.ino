f5c2bc0dfb77fafb1cf184796be27346ab382448
Guandi Chen <140076679+grandy0831@users.noreply.github.com>
2023-10-31 01:18:05 +0000

    Generate an HTML page
    Generate an HTML page with temperature and humidity information so that it can be visually displayed on the browser

---
+++ Generate_an_HTML_page/Generate_an_HTML_page.ino
@@ -0,0 +1,112 @@
+#include <ESP8266WiFi.h>  // Connect to a Wi-Fi network
+#include <ESP8266WebServer.h>  // Used to set up the HTTP server
+#include <ezTime.h>  // For processing date and time
+#include <DHT.h>
+#include <DHT_U.h>  // Used to interact with the DHT22 temperature and humidity sensor
+
+#define DHTTYPE DHT22   // The sensor type is defined as DHT22
+
+// DHT Sensor setup
+uint8_t DHTPin = 12;        // Pin 12 on the Huzzah connects to the DHT22 sensor
+DHT dht(DHTPin, DHTTYPE);   // Initialize DHT sensor.              
+float Temperature;          // Temperature value 
+float Humidity;             // Humidity value
+
+const char* ssid     = "xxx";
+const char* password = "xxx";
+
+ESP8266WebServer server(80);  // Create a network server instance that listens on port 80
+
+Timezone GB;  // Create a time zone object
+
+void setup() {
+  // open serial connection
+  Serial.begin(115200);
+  delay(100);
+
+  // start DHT sensor
+  pinMode(DHTPin, INPUT);
+  dht.begin(); 
+
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  // check to see if connected and wait until you are
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // when connected and IP address obtained start HTTP server  
+  server.on("/", handle_OnConnect);
+  server.onNotFound(handle_NotFound);
+  server.begin();
+  Serial.println("HTTP server started");
+
+  // get real date and time
+  waitForSync();
+  Serial.println("UTC: " + UTC.dateTime()); 
+  GB.setLocation("Europe/London");
+  Serial.println("London time: " + GB.dateTime());  
+
+}
+
+void loop() {
+  server.handleClient();  // Processing requests from clients
+  
+  //delay(1000);
+  //Serial.println(GB.dateTime("H:i:s")); // UTC.dateTime("l, d-M-y H:i:s.v T")
+}
+
+  // Get the temperature and humidity values for the DHT22 sensor 
+  // and then send these values as HTML pages to the client using the ‘server.send’ method
+void handle_OnConnect() {
+  Temperature = dht.readTemperature(); // Gets the values of the temperature
+  Humidity = dht.readHumidity(); // Gets the values of the humidity 
+  server.send(200, "text/html", SendHTML(Temperature,Humidity)); 
+}
+
+void handle_NotFound(){
+  server.send(404, "text/plain", "Not found");
+}
+
+
+// Build a simple HTML page 
+// Displays the temperature and humidity values detected by the DHT22 sensor
+String SendHTML(float Temperaturestat,float Humiditystat){
+  String ptr = "<!DOCTYPE html> <html>\n";  // Defines the basic structure of an HTML file, declares the document type and the HTML tag start
+  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";  // Set the viewport and zoom of the page displayed on the device
+  ptr +="<title>ESP8266 DHT22 Report</title>\n";  // Set page title
+  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";  // Using the Style Object
+  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";  // Set the style of the title
+  ptr +="p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";  // Sets the style of the paragraph
+  ptr +="</style>\n";
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+  ptr +="<div id=\"webpage\">\n";
+  ptr +="<h1>Plant Monitor Report</h1>\n";  // The main title of the page
+  
+  ptr +="<p>Temperature: ";
+  ptr +=Temperaturestat;
+  ptr +="degC</p>";
+  ptr +="<p>Humidity: ";
+  ptr +=(int)Humiditystat;
+  ptr +="%</p>";
+  ptr +="<p>Sampled on: ";
+  ptr +=GB.dateTime("l,");
+  ptr +="<br>";
+  ptr +=GB.dateTime("d-M-y H:i:s T");
+  ptr +="</p>";
+  
+  ptr +="</div>\n";
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+  return ptr;
+}
